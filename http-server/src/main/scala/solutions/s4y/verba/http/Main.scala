6717f878b1995c055eb450027371e2bc49dbc242
Sergey Dolin <sergey.dolin@gmail.com>
2025-11-05 18:54:20 +0100

    Refactor logging implementation to use Logger interface and improve debug output

--- http-server/src/main/scala/solutions/s4y/verba/http/Main.scala
+++ http-server/src/main/scala/solutions/s4y/verba/http/Main.scala
@@ -5,7 +5,7 @@ import cats.implicits.*
 import org.http4s.*
 import org.http4s.ember.server.EmberServerBuilder
 import org.http4s.server.middleware.{EntityLimiter, Logger as RequestLogger}
-import scribe.Scribe
+import scribe.Level
 import scribe.cats.LoggerExtras
 import solutions.s4y.verba.adapters.{GeminiRepository, OpenAIRepository}
 import solutions.s4y.verba.http.endpoints.modes.modesEndpoint
@@ -17,26 +17,36 @@ import solutions.s4y.verba.scribe.ScribeLogger
 import solutions.s4y.verba.usecases.TranslatorService
 
 object Main extends IOApp:
-  private val httpLogger: Scribe[IO] = scribe.Logger("verba.http4s").f[IO]
-  private val emberLogger = ScribeLogger.getLogger[IO]
 
   private val translatorService =
     TranslatorService(OpenAIRepository(), GeminiRepository())
 
-  def run(args: List[String]): IO[ExitCode] =
+  def run(args: List[String]): IO[ExitCode] = {
     ServerConfig.fromArgs(args) match {
       case Left(error) =>
         IO.println(error).as(ExitCode.Error)
       case Right(config) =>
+        scribe.Logger.root
+          .clearHandlers()
+          .clearModifiers()
+          .withHandler(minimumLevel = Some(Level.Debug))
+          .replace()
+
+        val httpLogger =
+          scribe.Logger("verba.http4s")
+
+        val httpLoggerIO = httpLogger.f[IO]
+
         val allRoutes: HttpRoutes[IO] =
           translationEndpoint(translatorService, httpLogger) <+>
             modesEndpoint(translatorService) <+>
             qualitiesEndpoint(translatorService) <+>
             providersEndpoint(translatorService)
 
-        val authenticatedRoutes: HttpRoutes[IO] = AuthMiddleware(config.secret)(
-          allRoutes
-        )
+        val authenticatedRoutes: HttpRoutes[IO] =
+          AuthMiddleware(config.secret)(
+            allRoutes
+          )
 
         val limitedRoutes: HttpRoutes[IO] = EntityLimiter.httpRoutes[IO](
           httpRoutes = authenticatedRoutes,
@@ -48,10 +58,10 @@ object Main extends IOApp:
             logHeaders = true,
             logBody = false,
             redactHeadersWhen = _ => false,
-            logAction = Some((msg: String) => httpLogger.info(msg))
+            logAction = Some((msg: String) => httpLoggerIO.info(msg))
           )(limitedRoutes.orNotFound)
 
-        httpLogger.info(
+        httpLoggerIO.info(
           s"Starting server on ${config.host}:${config.port}"
         ) *>
           EmberServerBuilder
@@ -59,19 +69,20 @@ object Main extends IOApp:
             .withHost(config.host)
             .withPort(config.port)
             .withHttpApp(translationApp)
-            .withLogger(emberLogger)
+            .withLogger(ScribeLogger.getLogger[IO])
             .build
             .use(_ =>
-              httpLogger.info(
+              httpLoggerIO.info(
                 s"Server is listening on ${config.host}:${config.port}"
               ) *> IO.never
             )
             .as(ExitCode.Success)
             .handleErrorWith { err =>
-              httpLogger
+              httpLoggerIO
                 .error(s"Server failed to start: ${err.getMessage}")
                 .as(ExitCode.Error)
             }
     }
+  }
   end run
 end Main
--- http-server/src/main/scala/solutions/s4y/verba/http/endpoints/translation/translationEndpoint.scala
+++ http-server/src/main/scala/solutions/s4y/verba/http/endpoints/translation/translationEndpoint.scala
@@ -7,20 +7,22 @@ import io.circe.generic.auto.*
 import org.http4s.HttpRoutes
 import org.http4s.circe.CirceEntityCodec.*
 import org.http4s.dsl.io.*
-import scribe.Scribe
+import scribe.cats.LoggerExtras
+import scribe.{Logger, Scribe}
 import solutions.s4y.verba.domain.vo.TranslationRequest
 import solutions.s4y.verba.usecases.TranslatorService
 
 def translationEndpoint(
     translationService: TranslatorService,
-    logger: Scribe[IO]
-): HttpRoutes[IO] =
+    logger: Logger
+): HttpRoutes[IO] = {
+  val loggerIO = logger.f[IO]
   HttpRoutes.of[IO] { case req @ POST -> Root / "translation" =>
     val result = for {
-      _ <- EitherT.right[String](logger.debug("Received translation request"))
+      _ <- EitherT.right[String](loggerIO.debug("Received translation request"))
       dto <- EitherT(req.as[TranslationRequestDto].attempt)
         .leftMap((err: Throwable) => s"Invalid JSON: ${err.getMessage}")
-      _ <- EitherT.right[String](logger.debug(s"Request DTO: $dto"))
+      _ <- EitherT.right[String](loggerIO.debug(s"Request DTO: $dto"))
       translationReq <- EitherT.fromEither[IO](
         TranslationRequest(
           sourceText = Some(dto.text),
@@ -32,12 +34,12 @@ def translationEndpoint(
         ).leftMap(err => err.message)
       )
       _ <- EitherT.right[String](
-        logger.debug(s"Translating from ${dto.from} to ${dto.to}")
+        loggerIO.debug(s"Translating from ${dto.from} to ${dto.to}")
       )
       translation <- EitherT(translationService.translate(translationReq))
         .leftMap(err => err.message)
       _ <- EitherT.right[String](
-        logger.debug("Translation completed successfully")
+        loggerIO.debug("Translation completed successfully")
       )
     } yield translation
 
@@ -48,3 +50,4 @@ def translationEndpoint(
         BadRequest(errorMsg)
     }
   }
+}
