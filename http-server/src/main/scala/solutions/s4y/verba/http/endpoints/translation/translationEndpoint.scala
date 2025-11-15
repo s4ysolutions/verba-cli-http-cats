f7b4092da987c3724d90f11f5e3b551dab04ce8f
Sergey Dolin <sergey.dolin@gmail.com>
2025-11-15 18:50:02 +0100

    Improve error handling and logging in translation endpoint

--- http-server/src/main/scala/solutions/s4y/verba/http/endpoints/translation/translationEndpoint.scala
+++ http-server/src/main/scala/solutions/s4y/verba/http/endpoints/translation/translationEndpoint.scala
@@ -7,8 +7,8 @@ import io.circe.generic.auto.*
 import org.http4s.HttpRoutes
 import org.http4s.circe.CirceEntityCodec.*
 import org.http4s.dsl.io.*
+import scribe.Logger
 import scribe.cats.LoggerExtras
-import scribe.{Logger, Scribe}
 import solutions.s4y.verba.domain.vo.TranslationRequest
 import solutions.s4y.verba.usecases.TranslatorService
 
@@ -21,7 +21,7 @@ def translationEndpoint(
     val result = for {
       _ <- EitherT.right[String](loggerIO.debug("Received translation request"))
       dto <- EitherT(req.as[TranslationRequestDto].attempt)
-        .leftMap((err: Throwable) => s"Invalid JSON: ${err.getMessage}")
+        .leftMap((err: Throwable) => err.getMessage)
       _ <- EitherT.right[String](loggerIO.debug(s"Request DTO: $dto"))
       translationReq <- EitherT.fromEither[IO](
         TranslationRequest(
@@ -39,16 +39,15 @@ def translationEndpoint(
       )
       translation <- EitherT(translationService.translate(translationReq))
         .leftMap(err => err.message)
-      _ <- EitherT.right[String](
-        loggerIO.debug("Translation completed successfully")
-      )
     } yield translation
 
     result.value.flatMap {
       case Right(translation) =>
-        Ok(translation)
+        loggerIO.debug("Translation completed successfully") *>
+          Ok(translation)
       case Left(errorMsg) =>
-        BadRequest(errorMsg)
+        loggerIO.error(s"Translation failed: $errorMsg") *>
+          BadRequest(errorMsg)
     }
   }
 }
